1b548136a2aa81b9f6557d3af797d7c251b5a4af
eason950710 <70698579+eason950710@users.noreply.github.com>
2021-01-27 13:11:36 +0800

    1_27 修正

--- 755523/9_17.cpp
+++ 755523/9_17.cpp
@@ -3,12 +3,13 @@ using namespace std;
  
 int main() { 
     cout << "Please input a number: "; 
+ 
     int num; 
     cin >> num; 
+ 
     if (num % 3 == 0) { 
         cout << num << "是3的倍數"; 
-    } 
-    else { 
+    } else { 
         cout << num << "不是3的倍數"; 
     } 
 } 
